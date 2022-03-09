cbd3170e9363ab31cbcd82d71101dc0c5564f087
kdy <ddragon0513@gmail.com>
2022-03-09 11:47:03 +0900

    불필요한 소스 제거

--- src/main/java/com/kdy/chat/controller/ChatAPIController.java
+++ src/main/java/com/kdy/chat/controller/ChatAPIController.java
@@ -63,103 +63,11 @@ public class ChatAPIController {
 	} 
 	
 	
-	/*
-	 * @author : KGH
-	 * @Date : 21.06.23
-	 * @Script : 
-	 * 			live 스케줄러에서 방송 종료시 종료된 방송의 채팅 기록을 text 확장자로 출력 
-	 * */
-	
-	/*
-	@CrossOrigin(origins="*")
-	@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, path = "/serviceTermination")
-	public void getRequest(@RequestParam Map<String, Object> param) throws Exception{
-		
-		java.util.Date date = new java.util.Date();
-		String year = new SimpleDateFormat("yyyy").format(new Date());
-		String month = new SimpleDateFormat("MM").format(new Date());
-		String day = new SimpleDateFormat("dd").format(new Date());
-		
-		String prefixFileName = year + "-" + month + "-" + day;
-		String chatFileName = "";
-	
-		
-		System.out.println(" param ==> " + param.toString());
-		
-		String lbSeq = param.get("lbSeq").toString();
-		String liveBroadcastTitle = param.get("liveBroadcastTitle").toString();
-
-		chatFileName = chatPath + year + "/" + month + "/" + prefixFileName + "_" + lbSeq + "_chat.txt";
-		
-		ChatBackUpDTO backUpDTO = (ChatBackUpDTO) hashOperations.get(ChatHashKey.BACKUP.toString(), lbSeq);
-	
-		File dir = new File(chatPath + year + "/" + month);
-		
-		if(!dir.isDirectory()) {
-			dir.mkdirs();
-		}
-		
-		File file = new File(chatFileName);
-		
-		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-		
-		writer.write(backUpDTO.getFullChat().toString());
-		
-		writer.close();
-	} 
-	*/
-	
-	/*
-	 * @author : KGH
-	 * @Date : 21.06.23
-	 * @Script : 
-	 * 			지정한 시간동안 파일 이어 쓰기  
-	 * */
-	
-	/*
-	@SuppressWarnings("unchecked")
-	@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, path = "/backupChatRequest")
-	public void backupChatRequest() throws Exception{
-		
-		java.util.Date date = new java.util.Date();
-		String year = new SimpleDateFormat("yyyy").format(new Date());
-		String month = new SimpleDateFormat("MM").format(new Date());
-		String day = new SimpleDateFormat("dd").format(new Date());
-		
-		String prefixFileName = year + "-" + month + "-" + day;
-		String chatFileName = "";
-	
-		File dir = new File(chatPath + year + "/" + month + "/backup/");
-		
-		if(!dir.isDirectory()) {
-			dir.mkdirs();
-		}
-	
-		Map<String, BlockingQueue<String>> backupChatRepo = systemDto.getRoomChatQueueMap();
-		
-		if(backupChatRepo != null) {
-			for(Map.Entry<String, BlockingQueue<String>> entry : backupChatRepo.entrySet()) {
-				chatFileName = chatPath + year + "/" + month + "/backup/" + prefixFileName + "_" + entry.getKey() + "_chat.txt";
-				File file = new File(chatFileName);
-				BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
-				
-				BlockingQueue<String> chatQueue = backupChatRepo.get(entry.getKey());
-				
-				while(!chatQueue.isEmpty()) {
-					writer.write(chatQueue.poll());
-				}
-				writer.close();
-				systemDto.getRoomChatQueueMap().remove(entry.getKey());
-			}
-		}
-	}
-	*/
-	
-	
 	//접속자수 가져오기
 	@RequestMapping("/getLiveViewsCount")
 	public LiveViewsDTO getLiveViewsCount(@RequestBody Map<String, Object> map) throws Exception{
 		
+		@SuppressWarnings("unchecked")
 		List<String> broadcastList = (List<String>)map.get("broadcastList");
 		
 		if(broadcastList == null || broadcastList.size() == 0) {
